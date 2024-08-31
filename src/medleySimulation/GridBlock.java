5c067ac1943985bd60d309559fbe0d90230b24c3
Zolile <zolilemikezoko@gmail.com>
2024-08-31 07:50:58 +0200

    Added source files

---
+++ src/medleySimulation/GridBlock.java
@@ -0,0 +1,61 @@
+//M. M. Kuttel 2024 mkuttel@gmail.com
+// GridBlock class to represent a block in the grid.
+// only one thread at a time "owns" a GridBlock - this must be enforced
+
+package medleySimulation;
+
+
+public class GridBlock {
+	
+	private int isOccupied; 
+	
+	private final boolean isStart;  //is this a starting block?
+	private int [] coords; // the coordinate of the block.
+	
+	GridBlock(boolean startBlock) throws InterruptedException {
+		isStart=startBlock;
+		isOccupied= -1;
+	}
+	
+	GridBlock(int x, int y, boolean startBlock) throws InterruptedException {
+		this(startBlock);
+		coords = new int [] {x,y};
+	}
+	
+	public synchronized  int getX() {return coords[0];}  
+	
+	public synchronized  int getY() {return coords[1];}
+	
+	
+	
+	//Get a block
+	public synchronized boolean get(int threadID) throws InterruptedException {
+		if (isOccupied==threadID) return true; //thread Already in this block
+		if (isOccupied>=0)return false; //space is occupied
+		isOccupied= threadID;  //set ID to thread that had block
+      notifyAll();
+		return true;
+	}
+		
+	
+	//release a block
+	public synchronized void release() {
+		isOccupied= -1;
+      notifyAll(); // notify sleeping threads that a block has been released.
+
+	}
+	
+
+	//is a bloc already occupied?
+	public synchronized boolean occupied() {
+		if(isOccupied==-1) return false;
+		return true;
+	}
+	
+	
+	//is a start block
+	public synchronized boolean isStart() {
+		return isStart;	
+	}
+
+}
